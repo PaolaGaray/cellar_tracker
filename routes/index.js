bee1988fa2a78ba53c5dd90925841b7444e6c667
Paola Garay <garay.angela@gmail.com>
2020-07-06 14:11:15 +0200

    model created

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,33 @@ const router  = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+  const user = req.user;
+
+  console.log('req.user:', req.user);
+  res.render('index', { user: user});
 });
+
+// create a middleware that checks if a user is logged in
+
+const loginCheck = () => {
+  return (req, res, next) => {
+    // instead of if (req.session.user) - passport uses req.isAuthenticated()
+    if (req.isAuthenticated()) {
+      // if user is logged in, proceed to the next function
+      next();
+    } else {
+      // else if user is not logged in, redirect to /login
+      res.redirect('/auth/login');
+    }
+  };
+};
+
+// router.get('/private', loginCheck(), (req, res) => {
+//   res.render('private');
+// });
+
+// router.get('/profile', loginCheck(), (req, res) => {
+//   res.render('profile');
+// });
 
 module.exports = router;
---
+++ routes/wines.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router  = express.Router();
+const Wine = require ('../models/Wine');
+const User = require ('../models/User');
+
