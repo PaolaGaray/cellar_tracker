483b98ed4c010cd678861c845da85e1f941d0357
Gabriel Palladini <gmpalladini@gmail.com>
2020-07-07 11:24:57 +0200

    fix conflit

--- routes/wines.js
+++ routes/wines.js
@@ -32,56 +32,53 @@ router.post('/add-wines-form', (req, res) => {
     console.log(error);
   })
 });
-
-//Delete Wines
-router.get('/wines/delete/:wineId', (req, res) => {
-  Wine.deleteOne({ _id: req.params.wineId })
-    .then(() => {
-      res.redirect('/wines/show');
-    })
-    .catch(err => {
-      console.log(err);
-      // next(err)
-    })
-})
-
-
-//HELP!
-//See wine details
-router.get('details/:id', (req, res, next) => {
-  Wine.findById(req.params.id)
-      .then(wine => {
-          res.render('/details', { wine: id });
-      })
-      .catch(err => {
-          next(err);
-      });
-});
+// //Delete Win
+// router.get('/wines/delete/:wineId', (req, res) => {
+//   Wine.deleteOne({ _id: req.params.wineId })
+//     .then(() => {
+//       res.redirect('/wines/show');
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       // next(err)
+//     })
+// })
 
 
-//HELP!
-//Update wine
-router.get('details/:id/edit', (req, res, next) => {
-  Wine.findById(req.params.id)
-      .then(wine => {
-          res.render('wines/edit', { wine });
-      })
-      .catch(err => {
-          next(err);
-      });
-});
-
-router.post('/:id', (req, res, next) => {
-  const { winery, name, type, year,  grape, country, region } = req.body;
-  Wine.findByIdAndUpdate(req.params.id, { winery, name, type, year,  grape, country, region })
-      .then(() => {
-          res.redirect('/wine');
-      })
-      .catch(err => {
-          next(err);
-      });
-})
+// //HELP!
+// //See wine details
+// router.get('details/:id', (req, res, next) => {
+//   Wine.findById(req.params.id)
+//       .then(wine => {
+//           res.render('/details', { wine: id });
+//       })
+//       .catch(err => {
+//           next(err);
+//       });
+// });
 
 
+// //HELP!
+// //Update wine
+// router.get('details/:id/edit', (req, res, next) => {
+//   Wine.findById(req.params.id)
+//       .then(wine => {
+//           res.render('wines/edit', { wine });
+//       })
+//       .catch(err => {
+//           next(err);
+//       });
+// });
+
+// router.post('/:id', (req, res, next) => {
+//   const { winery, name, type, year,  grape, country, region } = req.body;
+//   Wine.findByIdAndUpdate(req.params.id, { winery, name, type, year,  grape, country, region })
+//       .then(() => {
+//           res.redirect('/wine');
+//       })
+//       .catch(err => {
+//           next(err);
+//       });
+// })
 
 module.exports = router;
